270acb81a65d1a3a336180412dea76df516a0191
Martin Alfke <ma@example42.com>
2015-07-30 17:15:50 +0200

    use normal require 'spec_helper'
    https://trello.com/c/rldlAjHf/1087-pr-puppet-1692-21078-use-puppetlabs-spec-helper-garrett-honeycutt

--- spec/classes/activemq_spec.rb
+++ spec/classes/activemq_spec.rb
@@ -1,4 +1,5 @@
-require "#{File.join(File.dirname(__FILE__),'..','spec_helper.rb')}"
+require 'spec_helper'
+#require "#{File.join(File.dirname(__FILE__),'..','spec_helper.rb')}"
 
 describe 'activemq' do
 
